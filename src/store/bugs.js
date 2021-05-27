96db587160f31db931df51815d014273175e888b
Muhammad Zahid Masruri <masruri03@gmail.com>
2021-05-27 10:32:48 +0700

    refactored to create slice

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,27 +1,28 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-// Actions
-export const bugAdded = createAction("bugAdded");
-export const bugResolved = createAction("bugResolved");
-export const bugRemoved = createAction("bugRemoved");
-
-// Reducer
 let lastId = 0;
 
-export default createReducer([], {
-  [bugAdded.type]: (bugs, action) => {
-    bugs.push({
-      id: ++lastId,
-      description: action.payload.description,
-      resolved: false,
-    });
-  },
-  [bugResolved.type]: (bugs, action) => {
-    const index = bugs.findIndex((bug) => bug.id === action.payload.id);
-    bugs[index].resolved = true;
-  },
-  [bugRemoved.type]: (bugs, action) => {
-    const index = bugs.findIndex((bug) => bug.id === action.payload.id);
-    bugs.splice(index, 1);
+const slice = createSlice({
+  name: "bugs",
+  initialState: [],
+  reducers: {
+    bugAdded: (bugs, action) => {
+      bugs.push({
+        id: ++lastId,
+        description: action.payload.description,
+        resolved: false,
+      });
+    },
+    bugResolved: (bugs, action) => {
+      const index = bugs.findIndex((bug) => bug.id === action.payload.id);
+      bugs[index].resolved = true;
+    },
+    bugRemoved: (bugs, action) => {
+      const index = bugs.findIndex((bug) => bug.id === action.payload.id);
+      bugs.splice(index, 1);
+    },
   },
 });
+
+export default slice.reducer;
+export const { bugAdded, bugResolved, bugRemoved } = slice.actions;
